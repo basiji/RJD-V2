1422101c36111bdca08279d6efaaf5bdb0547083
basiji <40441882+basiji@users.noreply.github.com>
2018-09-04 10:19:20 +0430

    x

--- modules/router_v2.js
+++ modules/router_v2.js
@@ -43,7 +43,7 @@ module.exports = function(connection){
         // Set cookie
         res.cookie('userid', req.query.userid);
         res.cookie('plan', req.query.plan);
-        res.sendFile(__dirname + '/html/asan/index.htlm');
+        res.sendFile(__dirname + '/html/asan/index.html');
 
     });
 
